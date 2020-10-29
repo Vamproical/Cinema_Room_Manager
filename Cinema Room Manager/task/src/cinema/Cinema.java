6b16b871b07525cb99e398095eca60eb7b1eb5bc
Nick <mimoun136@yandex.ru>
2020-10-29 16:05:39 +1000

    4 stage finished.

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -3,9 +3,10 @@ package cinema;
 import java.util.Scanner;
 
 public class Cinema {
-    private final String[][] theater;
+    private final Scanner scanner = new Scanner(System.in);
+    private String[][] theater;
 
-    public Cinema(int rows, int column) {
+    private void fillCinema(int rows, int column) {
         theater = new String[column][rows];
         for (int i = 0; i < column; i++) {
             for (int j = 0; j < rows; j++) {
@@ -34,24 +35,42 @@ public class Cinema {
         theater[i - 1][j - 1] = "B";
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    private void menu() {
         System.out.println("Enter the number of rows:");
         int rows = scanner.nextInt();
         System.out.println("Enter the number of seats in each row:");
         int seatsRow = scanner.nextInt();
-        Cinema cinema = new Cinema(seatsRow,rows);
-        System.out.println("Cinema:");
-        cinema.printCinema();
-        System.out.println("Enter a row number:");
-        int row = scanner.nextInt();
-        System.out.println("Enter a seat number in that row:");
-        int seatRow = scanner.nextInt();
-        System.out.println();
-        System.out.println("Ticket price: $" + Price.priceSeat(rows,seatsRow,row));
-        cinema.setSeat(row,seatRow);
-        System.out.println();
-        System.out.println("Cinema:");
-        cinema.printCinema();
+        fillCinema(seatsRow,rows);
+        boolean flag = false;
+        while (!flag) {
+            System.out.println();
+            System.out.println("1. Show the seats\n" +
+                    "2. Buy a ticket\n" +
+                    "0. Exit");
+            int i = scanner.nextInt();
+            switch (i) {
+                case 1:
+                    System.out.println("Cinema:");
+                    printCinema();
+                    break;
+                case 2:
+                    System.out.println("Enter a row number:");
+                    int row = scanner.nextInt();
+                    System.out.println("Enter a seat number in that row:");
+                    int seatRow = scanner.nextInt();
+                    System.out.println();
+                    System.out.println("Ticket price: $" + Price.priceSeat(rows,seatsRow,row));
+                    setSeat(row,seatRow);
+                    break;
+                case 0:
+                    flag = true;
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Cinema cinema = new Cinema();
+        cinema.menu();
     }
 }
